cfcd2765e79dce28f059738aa1dcaf78e3625381
cheatchansovoth <57830826+cheatchansovoth@users.noreply.github.com>
2022-09-28 06:43:20 +1300

    update
    Login page

--- backend/app.js
+++ backend/app.js
@@ -1,34 +1,57 @@
 const express =require('express')
-const app= express();
-const {MongoClient} = require('mongodb');
+const cors=require('cors');
+const mongoose= require('mongoose');
 
+const app=express();
+app.use(express.json());
+app.use(cors());
 
-async function main()
+const mongoUrl="mongodb+srv://chansovoth:FMpO0En5ytbaKsi1@cluster0.n1ovkfy.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(mongoUrl,{
+    useNewUrlParser:true,
+}).then(()=>{
+    console.log('DB is conntected');
+}).catch((e)=>console.log(e));
+const port=5000;
+require('./userDetails');
+const User= mongoose.model('UserInformation');
+app.post('/register',async(req,res)=>
 {
-    const connection="mongodb+srv://chansovoth:FMpO0En5ytbaKsi1@cluster0.n1ovkfy.mongodb.net/?retryWrites=true&w=majority"
-    const client = new MongoClient(connection);
-    try{
-        await client.connect();
-        
-        console.log('DB is connected')
-
-    }
-    catch(e)
+  const name=req.body.name;
+  const email=req.body.email;
+  const password=req.body.email;
+  const signup= new User({name:name,email:email,password:password});
+  try
+  {
+    await signup.save();
+    res.send({status:"ok"});
+  }
+  catch(err)
+  {
+    console.log(err);
+    res.send({status:"error"});
+  }
+});
+app.post('/login',async(req,res,next)=>
+{
+    const email=req.body.email;
+    const password=req.body.password;
+    const user= await User.findOne({email:email});
+    if(!user || user.password!==password)
     {
-        console.error(e);
+        console.log('user not found')
     }
-    finally
+    else 
     {
-        await client.close();
+        console.log('user found');
     }
 }
-main().catch(console.error);
-
+);
 app.get('/',(req,res,next)=>
 {
-    res.send('<h1>Running at port 5000</h1>')
+    res.send(`Port is running at ${port}`);
 })
-app.listen(5000,function()
+app.listen(port,(req,res)=>
 {
-    console.log('Running at port 5000');
+    console.log(`App is running at port ${port}`);
 })
