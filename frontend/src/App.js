d3107da3aca73997dd033f55f12830e22ed3a1f4
cheatchansovoth <57830826+cheatchansovoth@users.noreply.github.com>
2022-10-01 21:18:25 -0700

    feat/login
    redesign css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,41 @@
 import Login from './Pages/Login'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button,Container,Nav,Navbar,NavDropdown,NavLink} from 'react-bootstrap';
+import {BrowserRouter as Router, Route, Routes,Link} from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Login/>
+      <Navbars/>
     </div>
   );
 }
-
+const Navbars=()=>{
+  return (
+    <Router>
+        <Navbar bg="dark" expand="lg" variant="dark">
+          <Container >
+            <Navbar.Brand as={Link} to="/">Home</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="text-center">
+                <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+                <Nav.Link as={Link} to="/about">About</Nav.Link>
+                <Nav.Link as={Link} to="/login">Login</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        <Routes>
+        <Route path='/' element={<Login/>}></Route>
+          {/* <Route path='/about' element={<About/>}></Route>
+          <Route path='/contact' element={<Contact/>}></Route>
+          <Route path='/login' element={<Login/>}></Route>
+          <Route path='/menu' element={<Menu/>}></Route>
+          <Route path='/register' element={<Register/>}></Route> */}
+        </Routes>
+  </Router>
+  );
+}
 export default App;
--- frontend/src/Pages/Login.js
+++ frontend/src/Pages/Login.js
@@ -1,73 +1,54 @@
 import React,{useState} from 'react';
 import {Link} from 'react-router-dom';
+import { Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Axios from 'axios';
 import {
-    MDBContainer,
-    MDBCol,
-    MDBRow,
-    MDBBtn,
-    MDBIcon,
-    MDBInput,
-    MDBCheckbox
-  }
-  from 'mdb-react-ui-kit';
+  MDBBtn,
+  MDBContainer,
+  MDBCard,
+  MDBCardBody,
+  MDBCardImage,
+  MDBRow,
+  MDBCol,
+  MDBInput,
+  MDBCheckbox
+}
+from 'mdb-react-ui-kit';
 import './Login.css';
 
 const Login=()=>
 {
-  const [email, setEmail]=useState("");
-  const [password, setPassword]=useState("");
-
-  const loginOnClick=()=>
-  {
-    Axios.post('http://localhost:5000/login',{email:email,password:password})
-  };
     return (
-<MDBContainer className="p-3 my-5 d-flex flex-column w-50">
+<MDBContainer className='login-section'>
+      <MDBCard >
+        <MDBRow className='g-0 d-flex align-items-center'>
+          <MDBCol md='4'>
+            <MDBCardImage src='https://cdni.iconscout.com/illustration/premium/thumb/boy-doing-homework-5796181-4841276.png' alt='phone' className='rounded-t-5 rounded-tr-lg-0' fluid />
+          </MDBCol>
 
-<MDBInput wrapperClass='mb-4' label='Email address' id='form1' type='email'
-            onChange={(event)=>{
-                setEmail(event.target.value)
-              }}
-/>
-<MDBInput wrapperClass='mb-4' label='Password' id='form2' type='password'
-            onChange={(event)=>{
-                setPassword(event.target.value)
-                }}  />
+          <MDBCol md='8'>
 
-<div className="d-flex justify-content-between mx-3 mb-4">
-  <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Remember me' />
-  <a href="!#">Forgot password?</a>
-</div>
+            <MDBCardBody>
+              <h1>Login</h1>
+              <MDBInput wrapperClass='mb-4' label='Email address' id='form1' type='email'/>
+              <MDBInput wrapperClass='mb-4' label='Password' id='form2' type='password'/>
 
-<MDBBtn className="mb-4" onClick={loginOnClick}>Sign in</MDBBtn>
+              <div className="d-flex justify-content-between mx-4 mb-4">
+                <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Remember me' />
+                <a href="!#">Forgot password?</a>
+              </div>
 
-<div className="text-center">
-  <p>Not a member? <a href="#!">Register</a></p>
-  <p>or sign up with:</p>
+              <MDBBtn className="mb-4 w-50"  color='dark'>Sign in</MDBBtn>
 
-  <div className='d-flex justify-content-between mx-auto' style={{width: '40%'}}>
-    <MDBBtn tag='a' color='none' className='m-1' style={{ color: '#1266f1' }}>
-      <MDBIcon fab icon='facebook-f' size="sm"/>
-    </MDBBtn>
+            </MDBCardBody>
 
-    <MDBBtn tag='a' color='none' className='m-1' style={{ color: '#1266f1' }}>
-      <MDBIcon fab icon='twitter' size="sm"/>
-    </MDBBtn>
+          </MDBCol>
 
-    <MDBBtn tag='a' color='none' className='m-1' style={{ color: '#1266f1' }}>
-      <MDBIcon fab icon='google' size="sm"/>
-    </MDBBtn>
+        </MDBRow>
 
-    <MDBBtn tag='a' color='none' className='m-1' style={{ color: '#1266f1' }}>
-      <MDBIcon fab icon='github' size="sm"/>
-    </MDBBtn>
-
-  </div>
-</div>
-
-</MDBContainer>
-       
-       )
-};
+      </MDBCard>
+    </MDBContainer>
+  );
+      }
 export default Login;
