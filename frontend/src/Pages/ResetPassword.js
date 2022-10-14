193eed031557883deaa1683937a66e65bd34bf20
Chickenboyray <80376080+Chickenboyray@users.noreply.github.com>
2022-10-14 19:33:36 +1300

    Revert "Feat/searchbar"

--- frontend/src/Pages/ResetPassword.js
+++ frontend/src/Pages/ResetPassword.js
@@ -11,13 +11,7 @@ import Axios from 'axios'
 const ResetPassword=()=>
 
 {
-<<<<<<< HEAD
-    const [error,setError]=useState("");
-    const [success,setSuccess]=useState("");
-
-=======
     const [getToken,setGetToken]=useState("");
->>>>>>> 8b65d53c0426304c731e3e4a629d6835febee189
     const Formik=useFormik({
         initialValues:{
             email:'',
@@ -27,18 +21,9 @@ const ResetPassword=()=>
                 email:values.email,
             }).then((res)=>
             {
-<<<<<<< HEAD
-                console.log(res.data);
-                setSuccess("Please check your email.")
-            }).catch((err)=>
-            {
-                // console.log(err.response.data.error);
-                setError(err.response.data.error)
-=======
                 window.localStorage.setItem('token',JSON.stringify(res.data));
                 console.log(res.data);
                 setGetToken(res.data);
->>>>>>> 8b65d53c0426304c731e3e4a629d6835febee189
             })
         }
     })
@@ -51,13 +36,8 @@ const ResetPassword=()=>
                 <MDBInput placeholder='Email' className='w-50 submit-field mt-5' type='email' name='email' value={Formik.values.email} onChange={Formik.handleChange}></MDBInput>
                 </Col>
                 <Col lg={12}>
-<<<<<<< HEAD
-                {error?<p className='error-class'>{error}</p>:null}
-                {success?<p className='success-class'>{success}</p>:null}
-=======
                 {/* {error?<p className='error-class'>{error}</p>:null}
                 {success?<p className='success-class'>{success}</p>:null} */}
->>>>>>> 8b65d53c0426304c731e3e4a629d6835febee189
                 </Col>
                 <Col lg={12}>
                 <MDBBtn type='submit' className='submit-btn mt-5'>Submit</MDBBtn>
@@ -67,12 +47,9 @@ const ResetPassword=()=>
                 </Col>
             </Row>
             </form>
-<<<<<<< HEAD
-=======
             <Link to={`newpassword/${getToken._id}`}>
                         <MDBBtn variant="primary" className='m-1'>Edit</MDBBtn>
             </Link>
->>>>>>> 8b65d53c0426304c731e3e4a629d6835febee189
             </Container>
     </div>
 }
