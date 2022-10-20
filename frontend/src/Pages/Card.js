6416ef16f86ebf6ecfc092d9ed359cca5e797bb0
cheatchansovoth <57830826+cheatchansovoth@users.noreply.github.com>
2022-10-19 20:56:25 -0700

    update search
    searching article using title and name

--- frontend/src/Pages/Card.js
+++ frontend/src/Pages/Card.js
@@ -7,24 +7,40 @@ const CartContent=()=>
 {
 
     const [cardDetails,setCardDetails]=useState([]);
+    const [search,setSearch]=useState("");
     useEffect(()=>
     {
         Axios.get("http://localhost:5000/cardinformation").then((response)=>
         {
             setCardDetails(response.data);
-            console.log(response.data)
         })
     },[])
     return (
         <div className='mt-5'>
-            <h1>Article</h1>
+            <h1>Search Article</h1>
+            <input type='text' onChange={(event)=>
+            {
+                setSearch(event.target.value);
+            }}></input>
             <div className='.Card'>
             <Container>
             <Row>
-                {cardDetails.map((val,key)=>
+                {cardDetails.filter((val)=>
+                {
+                    let searchAuthor=val.authors.toLowerCase().includes(search.toLocaleLowerCase())
+                    let searchTitle=val.title.toLowerCase().includes(search.toLocaleLowerCase())
+                    if(search=="")
+                    {
+                        return val
+                    }
+                    else if ( searchAuthor|| searchTitle)
+                    {
+                        return val
+                    }
+                }).map((val,key)=>
                     {
                         return(
-                            <Card>
+                            <Card key={key}>
                             <Card.Body><h5>{val.title}</h5>
                             <p><i>{val.authors}</i></p>
                             <p><i>{val.pubyear}</i></p>
