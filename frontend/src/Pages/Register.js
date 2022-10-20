6ad0ae497d01c904a6336a522bc8642425cb1c25
cheatchansovoth <57830826+cheatchansovoth@users.noreply.github.com>
2022-10-20 01:42:11 -0700

    update css

--- frontend/src/Pages/Register.js
+++ frontend/src/Pages/Register.js
@@ -47,13 +47,13 @@ const Register=()=>
         <MDBCardBody className='px-5'>
           <h2 className="text-uppercase text-center mb-5">Register</h2>
           <MDBInput wrapperClass='mb-4' placeholder='Your Name' size='lg' id='form1' type='text' name='name' className={`${Formik.touched.name && Formik.errors.name}`} value={Formik.values.name} onChange={Formik.handleChange}/>
-          {Formik.errors.name &&Formik.touched.name ? <p>{Formik.errors.name}</p>:null}
+          {Formik.errors.name &&Formik.touched.name ? <p className=' error-class'>{Formik.errors.name}</p>:null}
           <MDBInput wrapperClass='mb-4' placeholder='Email' size='lg' id='form2' type='email' name='email' className={`${Formik.touched.email && Formik.errors.email}`} value={Formik.values.email} onChange={Formik.handleChange}/>
-          {Formik.errors.email &&Formik.touched.email ? <p>{Formik.errors.email}</p>:null}
+          {Formik.errors.email &&Formik.touched.email ? <p className=' error-class'>{Formik.errors.email}</p>:null}
           <MDBInput wrapperClass='mb-4' placeholder='Password' size='lg' id='form3' type='password' name='password' className={`${Formik.touched.password && Formik.errors.password}`} value={Formik.values.password} onChange={Formik.handleChange}/>
-          {Formik.errors.password &&Formik.touched.password ? <p>{Formik.errors.password}</p>:null}
+          {Formik.errors.password &&Formik.touched.password ? <p className=' error-class'>{Formik.errors.password}</p>:null}
           <MDBInput wrapperClass='mb-4' placeholder='Confirm' size='lg' id='form4' type='password' name='confirm' className={`${Formik.touched.confirm && Formik.errors.confirm}`} value={Formik.values.confirm} onChange={Formik.handleChange}/>
-          {Formik.errors.confirm &&Formik.touched.confirm ? <p>{Formik.errors.confirm}</p>:null}
+          {Formik.errors.confirm &&Formik.touched.confirm ? <p className=' error-class'>{Formik.errors.confirm}</p>:null}
           <div className='d-flex flex-row justify-content-center mb-4'>
             <MDBCheckbox name='flexCheck' id='flexCheckDefault' label='I agree all statements in Terms of service' />
           </div>
